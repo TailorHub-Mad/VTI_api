28069e501822814eb26cbd17da68e763fac86080
Kunry <kunry3@hotmail.com>
2021-10-29 10:50:53 +0200

    fix: update filter

--- src/router/filter.route.ts
+++ src/router/filter.route.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { IFilterDocument } from '../interfaces/models.interface';
 import { FilterModel } from '../models/filter.model';
 import { createFilterValidation } from '../validations/filter.validation';
-import { Create, DeleteCrud } from '../controllers/crud.controller';
+import { Create, DeleteCrud, Update } from '../controllers/crud.controller';
 import { GetFilters } from '../controllers/filter.controller';
 
 const router = Router();
@@ -12,6 +12,8 @@ router.get('/simple', GetFilters({ type: 'simple' }));
 router.get('/complex', GetFilters({ type: 'complex' }));
 
 router.post('/', Create<IFilterDocument>(FilterModel, createFilterValidation));
+
+router.put('/:id', Update<IFilterDocument>(FilterModel, createFilterValidation));
 
 router.delete('/delete/:id', DeleteCrud<IFilterDocument>(FilterModel));
 
